027f5cc0e51c631ce4bdd3c5e7048ff4cc97c916
yf01-web <yeisonfarfan01@gmail.com>
2025-12-02 22:03:04 -0500

    un pequeño cambio en la app core

--- core/views.py
+++ core/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from recursos.models import Recurso
 
+#home pagina publica antes de iniciar sesion (login) donde se muestra el catalogo de algunos recursos que 
+# presta el sistema interuniversitario IntegraServicios, es decir es una vista preliminar antes de loguearse
 
 def home(request):
     recursos = Recurso.objects.all()[:8]  # mostrar solo 8 recursos en portada
@@ -10,37 +12,5 @@ def home(request):
         'recursos': recursos
     }
     return render(request, 'core/home.html', context)
-
-# def login_view(request):
-#     if request.method == "POST":
-#         username = request.POST.get("username")
-#         password = request.POST.get("password")
-
-#         user = authenticate(request, username=username, password=password)
-
-#         if user:
-#             login(request, user)
-#             return redirect("home")
-
-#     return render(request, "core/login.html")
-
-# def dashboard(request):
-#     return render(request, 'core/dashboard.html')
-
-# def register(request):
-#     if request.method == "POST":
-#         User.objects.create_user(
-#             username=request.POST["username"],
-#             email=request.POST["email"],
-#             password=request.POST["password1"],
-#             first_name=request.POST["first_name"],
-#             last_name=request.POST["last_name"],
-#         )
-#         return redirect("login")
-
-#     return render(request, "core/register.html")
-
-# def register_view(request):
-#     return render(request, "core/register.html")
 
 
