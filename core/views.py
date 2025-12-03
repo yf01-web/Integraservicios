8c228f9b74305f133504ca0c36cc75ac959cdc26
yf01-web <yeisonfarfan01@gmail.com>
2025-12-02 23:41:34 -0500

    modificacion views.py en core

--- core/views.py
+++ core/views.py
@@ -2,6 +2,23 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from recursos.models import Recurso
+from .decorators import cliente_requerido, empleado_requerido, administrador_requerido
+
+# Ejemplo de vista solo para clientes
+@cliente_requerido
+def nueva_reserva(request):
+    return render(request, 'core/nueva_reserva.html')
+
+# Ejemplo de vista solo para empleados  
+@empleado_requerido
+def registrar_prestamo(request):
+    return render(request, 'core/registrar_prestamo.html')
+
+# Ejemplo de vista solo para administradores
+@administrador_requerido
+def gestion_usuarios(request):
+    return render(request, 'core/gestion_usuarios.html')
+
 
 #home pagina publica antes de iniciar sesion (login) donde se muestra el catalogo de algunos recursos que 
 # presta el sistema interuniversitario IntegraServicios, es decir es una vista preliminar antes de loguearse
